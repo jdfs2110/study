4d453c4d3b7a6ac75bdcd820014c3002fa7c6b0c
jdfs2110 <117124266+jdfs2110@users.noreply.github.com>
2023-01-12 23:35:43 +0100

    deleting shit cuz idk how to overwrite it :D

--- Programacion/Cobra+/PackCobra/PackCobra.java
+++ Programacion/Cobra+/PackCobra/PackCobra.java
@@ -40,6 +40,90 @@ public class PackCobra {
     }
 
     public static double kWConsumidos(){
-        Scanner sc = new Scanner(System.in)
+        Scanner sc = new Scanner(System.in);
+        boolean correcto;
+        double kW;
+        do{
+        correcto = false;
+        System.out.println("Introduce los kW consumidos");
+        kW = sc.nextDouble();
+        if(kW >= 0){
+            correcto = true;
+        }else{
+            System.out.println("Los kW no son correctos");
+        }
+        }while(!correcto);
+        sc.close();
+        return kW;
+    }
+
+    /*public static String calculaImporte(double kW, String fecha){ //no esta bien creo
+        double importe = 0;
+        String kWs = String.valueOf(kW);
+        do {
+            switch(kWs){
+                case "2.30":
+                    importe = kW * 7.29;
+                    break;
+                case "3.45":
+                    importe = kW * 10.94;
+                    break;
+                case "4.60":
+                    importe = kW * 14.58;
+                    break;
+                case "5.75":
+                    importe = kW * 18.23;
+                    break;
+                case "6.90":
+                    importe = kW * 21.87;
+                    break;
+                case "8.05":
+                    importe = kW * 25.52;
+                    break;
+                default:
+                    System.out.println("La potencia no es correcta");
+                    break;
+            }
+        } while (kW != 2.30 || kW != 3.45 || kW != 4.60 || kW != 5.75 || kW != 6.90 || kW != 8.05);
+        return String.valueOf(importe) + "€";
+    }*/
+
+    public static String calculaImporte(double kW, String fechaLecturaAnterior, String fechaLecturaActual, double potenciaContratada){
+        int diaLecturaAnterior = Integer.parseInt(fechaLecturaAnterior.substring(0, 2));
+        int mesLecturaAnterior = Integer.parseInt(fechaLecturaAnterior.substring(3, 5));
+        int yearLecturaAnterior = Integer.parseInt(fechaLecturaAnterior.substring(6));
+        int diaLecturaActual = Integer.parseInt(fechaLecturaActual.substring(0, 2));
+        int mesLecturaActual = Integer.parseInt(fechaLecturaActual.substring(3, 5));
+        int yearLecturaactual = Integer.parseInt(fechaLecturaActual.substring(6));
+        double importe;
+        double preciokWHora = 0.37204;
+        double preciokWDia = preciokWHora  * 24;
+        double preciokWSemana = preciokWDia * 7;
+        String potenciaContratadaStr = String.valueOf(potenciaContratada);
+        do {
+            switch(potenciaContratadaStr){
+                case "2.30":
+                    importe = kW * 7.29;
+                    break;
+                case "3.45":
+                    importe = kW * 10.94;
+                    break;
+                case "4.60":
+                    importe = kW * 14.58;
+                    break;
+                case "5.75":
+                    importe = kW * 18.23;
+                    break;
+                case "6.90":
+                    importe = kW * 21.87;
+                    break;
+                case "8.05":
+                    importe = kW * 25.52;
+                    break;
+                default:
+                    System.out.println("La potencia no es correcta, introducela de nuevo");
+                    break;
+            }
+        } while (kW != 2.30 || kW != 3.45 || kW != 4.60 || kW != 5.75 || kW != 6.90 || kW != 8.05);
     }
 }
